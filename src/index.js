f92ca36ac0a7078a9a889e1977c4e4a1273a5a1e
rjbma <ricardo.araujo@obconnect.io>
2026-05-14 10:53:14 +0100

    chore: remove console.log

--- src/index.js
+++ src/index.js
@@ -62,7 +62,6 @@ async function generateKey(alg) {
 
 async function signJwt({ header, payload, privateKeyPem }) {
   try {
-    console.log(privateKeyPem)
     const privateKey = await jose.importPKCS8(privateKeyPem, header.alg);
     const jwt = await new jose.SignJWT(payload).setProtectedHeader(header).sign(privateKey);
     return jwt;
