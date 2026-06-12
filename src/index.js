98bc7b6548ab8cf113ddf0edf356614a19dd1ade
rjbma <ricardo.araujo@obconnect.io>
2026-06-12 10:52:51 +0100

    feat: added an endpoint returning an empty CRL

--- src/index.js
+++ src/index.js
@@ -21,15 +21,20 @@ export default {
           headers: { 'Content-Type': 'application/json' },
         });
       } else if (url.pathname == '/jwt' && request.method == 'POST') {
-        if (request.headers.get("Content-Type") !== "application/json") {
-          return new Response(JSON.stringify({ error: "Expected JSON" }), {
+        if (request.headers.get('Content-Type') !== 'application/json') {
+          return new Response(JSON.stringify({ error: 'Expected JSON' }), {
             status: 415,
-            headers: { "Content-Type": "application/json" },
+            headers: { 'Content-Type': 'application/json' },
           });
         }
         const { privateKey, header, payload } = await request.json();
         const jwt = await signJwt({ header, payload, privateKeyPem: privateKey });
         return new Response(jwt);
+      } else if (url.pathname == '/crl' && request.method === 'GET') {
+        const crl = await getEmptyCRL();
+        return new Response(crl, {
+          headers: { 'Content-Type': 'application/pkix-crl' },
+        });
       } else if (url.pathname == '/health' && request.method === 'GET') {
         return new Response(JSON.stringify({ status: 'ok' }), {
           headers: { 'Content-Type': 'application/json' },
@@ -69,4 +74,19 @@ async function signJwt({ header, payload, privateKeyPem }) {
     console.error(err);
     throw new Error('Failed to sign JWT');
   }
+}
+
+async function getEmptyCRL() {
+  return `-----BEGIN X509 CRL-----
+MIIBujCBowIBATANBgkqhkiG9w0BAQsFADA/MQswCQYDVQQGEwJERTEWMBQGA1UE
+CgwNVGVzdCBQU0QyIFBLSTEYMBYGA1UEAwwPVGVzdCBJc3N1aW5nIENBFw0yNjA2
+MTAwODIzMTRaFw0yNjA2MTcwODIzMTRaoDAwLjAfBgNVHSMEGDAWgBSDIMB67J8L
+/vl5PhDpDUtd+ZDLJzALBgNVHRQEBAICEAAwDQYJKoZIhvcNAQELBQADggEBAG2s
+B2G4iHcf2nOKHvdYTB/Nhb8xZFCOzH/q3YO4CXLKs+Q+jJ2ZbTe8GMCJC6V9AmE5
+0Cn3bHYU7ycS6DipDWBB4SjfqJrJ5Znvi4q7JqKIn5H4lWabUHv0g9QKFJUknQm0
+j0DQ4xjiLvDBnNlu7QV2PhdWBBA7humqlMvsXuthzg+nhvq5Emj0h9Z3d4OiXn7v
+so0l+Iyo1fix6SSTY5tvHMQo/P9yhA7TfULIoNku9BzMOV2bbtiWwDEqCALwsq2c
+gcExQesaSMhOkby0OpzZ9RHUT7vUDQz/MdTpEguBNxS+XC029Dbgo17/6lSmDrma
+zfyF3aDtB4G04Uln3mw=
+-----END X509 CRL-----`;
 }
